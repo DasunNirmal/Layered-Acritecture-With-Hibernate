455f91db09000cad795931a6a2a3de8f5735d48c
DasunNirmal <dasun200301@gmail.com>
2024-03-07 15:09:20 +0530

    Search done

--- src/main/java/lk/ijse/dao/custom/impl/StudentDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/StudentDAOImpl.java
@@ -5,8 +5,11 @@ import lk.ijse.dao.custom.StudentDAO;
 import lk.ijse.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO {
 
@@ -39,18 +42,19 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public Student search(String id) throws SQLException, ClassNotFoundException {
-        /*ResultSet resultSet = SQLUtil.execute("SELECT * FROM Student WHERE id = ?",id);
-
         Student entity = null;
-        if (resultSet.next()) {
-            int Sid = Integer.parseInt(resultSet.getString(1));
-            String name = resultSet.getString(2);
-            String address = resultSet.getString(3);
-            int phoneNumber = Integer.parseInt(resultSet.getString(4));
 
-            entity = new Student(Sid,name,address,phoneNumber);
+        /*We're binding the parameter id using setParameter.*/
+        /*We're retrieving the result as a list of Student objects directly using getResultList().*/
+
+        Query<Student> query = session.createQuery("FROM Student WHERE id = :id", Student.class);
+        query.setParameter("id", id);
+        List<Student> resultList = query.getResultList();
+
+        if (!resultList.isEmpty()) {
+            entity = resultList.get(0);
         }
-        return entity;*/
-        return null;
+
+        return entity;
     }
 }
